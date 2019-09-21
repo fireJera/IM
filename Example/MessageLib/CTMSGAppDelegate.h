63591e07a07337e608749f642a166103f5a77e26
Jeremy <>
2019-09-21 15:54:51 +0800

    Initial commit

---
+++ Example/MessageLib/CTMSGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CTMSGAppDelegate.h
+//  MessageLib
+//
+//  Created by r913218338@163.com on 09/21/2019.
+//  Copyright (c) 2019 r913218338@163.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CTMSGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
